1bb145372b442093be5f5666f10d059f47c1e019
TangLian <96833954+LiangT721@users.noreply.github.com>
2022-10-15 10:48:28 -0600

    add select signal

--- src/components/SearchingPart.jsx
+++ src/components/SearchingPart.jsx
@@ -21,19 +21,20 @@ const SearchingPart = (props) => {
 
   const { setDisplayList, setIsDefaultList, isNora } = props;
   const eng = lanState.lan;
-  const ref = useRef()
+  const ref = useRef();
 
   const handleChange = (e) => {
     setCondition(e.target.value);
   };
   const search = async (el) => {
+    console.log(el);
+    setSearchContent(el);
     const content = el.replaceAll(" ", "%20");
     try {
       const res = await sendRequest(
         `${URL}api/paintings/search/${uid}^${content}`,
         "GET"
       );
-      console.clear();
       setDisplayList(res.paintingList);
       setIsDefaultList(false);
     } catch (err) {
@@ -42,6 +43,7 @@ const SearchingPart = (props) => {
   };
 
   const categorySearch = async (el) => {
+    setCondition("");
     setSearchContent(el);
     const content = el.replaceAll(" ", "%20");
     try {
@@ -70,14 +72,20 @@ const SearchingPart = (props) => {
     };
     fetchData();
   }, [sendRequest, uid]);
-  console.log(offset);
+  console.log(searchContent);
   return (
-    <div ref={ref} className={`painting-searching-part  ${props.className} ${offset>200&&"painting-search__topStick"}`}>
+    <div
+      ref={ref}
+      className={`painting-searching-part  ${props.className} ${
+        offset > 200 && "painting-search__topStick"
+      }`}
+    >
       <div className="painting__searchingbar ">
         <input
           type="text"
           className="painting__searchinput"
           onChange={handleChange}
+          value={condition}
           placeholder={eng ? "Search content" : "请输入您要搜索的内容"}
         />
         <button
@@ -87,7 +95,11 @@ const SearchingPart = (props) => {
           Search
         </button>
       </div>
-      <div className={`painting__categorys ${offset>200&&"painting-search__topStick__category"}`}>
+      <div
+        className={`painting__categorys ${
+          offset > 200 && "painting-search__topStick__category"
+        }`}
+      >
         {category.map((el) => (
           <div
             className={`painting__category ${
@@ -106,13 +118,28 @@ const SearchingPart = (props) => {
           </div>
         ))}
       </div>
-      <div className={`painting__keywords ${offset>200&&"painting-search__topStick__Keywords"}`}>
+      <div
+        className={`painting__keywords ${
+          offset > 200 && "painting-search__topStick__Keywords"
+        }`}
+      >
         {keyWords.length > 0 &&
           keyWords.map((el) => (
             <div
-              className="painting__keyword"
+              className={`painting__keyword ${
+                el._id === searchContent &&
+                !isNora &&
+                "painting__keyword__selected"
+              } ${
+                el._id === searchContent &&
+                isNora &&
+                "painting__keyword__nora__selected"
+              }`}
               key={el._id}
-              onClick={() => search(el._id)}
+              onClick={() => {
+                setCondition("");
+                search(el._id);
+              }}
             >
               <Text>{el._id}</Text>
             </div>
